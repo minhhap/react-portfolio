a6c0d2073361c7fd5ed89322198063f1fd7f1755
minhhap <69835497+minhhap@users.noreply.github.com>
2021-11-03 23:33:39 -0700

    Updates

---
+++ precache-manifest.f16aafe462cab3567454001845bd1a0d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d033ae8719d5701f7ec7dce801afbc",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4df4860b081c35da29ca",
+    "url": "/react-portfolio/static/css/main.b98ea3e8.chunk.css"
+  },
+  {
+    "revision": "162642d6c232cf206d5f",
+    "url": "/react-portfolio/static/js/2.10033738.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.10033738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df4860b081c35da29ca",
+    "url": "/react-portfolio/static/js/main.97db19e7.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "3d1b5cf2e709ddf7b6ce54408ca86aaa",
+    "url": "/react-portfolio/static/media/0.3d1b5cf2.jpg"
+  },
+  {
+    "revision": "3adfa0c73972c6e28a818a0009140582",
+    "url": "/react-portfolio/static/media/1.3adfa0c7.jpg"
+  },
+  {
+    "revision": "08109467871dd2024f23ece75efed58c",
+    "url": "/react-portfolio/static/media/2.08109467.jpg"
+  },
+  {
+    "revision": "92599e67f002f97bea9955a334491019",
+    "url": "/react-portfolio/static/media/3.92599e67.jpg"
+  },
+  {
+    "revision": "34f59f1ed8cc1a5061538f50ebd6afe7",
+    "url": "/react-portfolio/static/media/4.34f59f1e.jpg"
+  },
+  {
+    "revision": "95d7b39967ece5c88465a9a749da1458",
+    "url": "/react-portfolio/static/media/cover-image.95d7b399.jpg"
+  }
+]);
